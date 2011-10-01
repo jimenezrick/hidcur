c8d85115e6f23dee716835e0c212555fef0d6f09
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2011-10-01 17:45:37 +0200

    Changes to using x_connection_t

--- hidcur.c
+++ hidcur.c
@@ -7,79 +7,125 @@
 
 #include <xcb/xcb.h>
 
-#define MOUSE_MASK XCB_EVENT_MASK_POINTER_MOTION | \
-		   XCB_EVENT_MASK_BUTTON_PRESS   | \
-		   XCB_EVENT_MASK_BUTTON_RELEASE
+#define MOUSE_MASK  XCB_EVENT_MASK_POINTER_MOTION | \
+		    XCB_EVENT_MASK_BUTTON_PRESS   | \
+		    XCB_EVENT_MASK_BUTTON_RELEASE
+#define CURSOR_CHAR 68
 
-static void error(const char *msg, xcb_connection_t *conn);
-static xcb_connection_t *connect(int *screen_pref);
-static xcb_screen_t *get_screen(xcb_connection_t *conn, int screen_num);
-static int grab_pointer(xcb_connection_t *conn, xcb_window_t win);
+typedef struct {
+	xcb_connection_t *conn;
+	xcb_screen_t     *screen;
+	int               screen_num;
+} x_connection_t;
 
-void error(const char *msg, xcb_connection_t *conn)
+
+// XXX XXX XXX
+// static ...
+static void x_disconnect(x_connection_t xconn);
+static void set_screen(x_connection_t *xconn);
+// XXX XXX XXX
+
+
+void error(const char *msg, x_connection_t xconn)
 {
 	fprintf(stderr, "Error: %s\n", msg);
-	if (conn) xcb_disconnect(conn);
+	if (xconn.conn) x_disconnect(xconn);
 
 	exit(EXIT_FAILURE);
 }
 
-xcb_connection_t *connect(int *screen_pref)
+x_connection_t x_connect(void)
 {
-	xcb_connection_t *conn;
+	x_connection_t xconn;
 
-	conn = xcb_connect(NULL, screen_pref);
-	if (!conn) error("couldn't connect to the server", NULL);
+	xconn.conn = xcb_connect(NULL, &xconn.screen_num);
+	if (!xconn.conn) error("couldn't connect to server", xconn);
+	set_screen(&xconn);
 
-	return conn;
+	return xconn;
 }
 
-xcb_screen_t *get_screen(xcb_connection_t *conn, int screen_num)
+void x_disconnect(x_connection_t xconn)
+{
+	xcb_disconnect(xconn.conn);
+}
+
+void set_screen(x_connection_t *xconn)
 {
 	xcb_screen_iterator_t it;
-	xcb_screen_t         *screen = NULL;
 
-	it = xcb_setup_roots_iterator(xcb_get_setup(conn));
-	for (; it.rem; xcb_screen_next(&it), screen_num--) {
-		if (!screen_num) {
-			screen = it.data;
+	xconn->screen = NULL;
+	it = xcb_setup_roots_iterator(xcb_get_setup(xconn->conn));
+	for (; it.rem; xcb_screen_next(&it), xconn->screen_num--) {
+		if (!xconn->screen_num) {
+			xconn->screen = it.data;
 			break;
 		}
 	}
-	if (!screen) error("couldn't find the screen", conn);
-
-	return screen;
+	if (!xconn->screen) error("couldn't find screen", *xconn);
 }
 
-int grab_pointer(xcb_connection_t *conn, xcb_window_t win)
+bool grab_pointer(x_connection_t xconn)
 {
 	xcb_grab_pointer_cookie_t cookie;
 	xcb_grab_pointer_reply_t *reply;
 	xcb_generic_error_t      *err;
 
 	// TODO: probar owner_events = true, confine_to = root_win
-	cookie = xcb_grab_pointer(conn, false, win, MOUSE_MASK, XCB_GRAB_MODE_ASYNC,
-				  XCB_GRAB_MODE_ASYNC, XCB_WINDOW_NONE, XCB_CURSOR_NONE,
-				  XCB_TIME_CURRENT_TIME);
-	reply = xcb_grab_pointer_reply(conn, cookie, &err);
-	if (err) error("couldn't grab the pointer", conn);
+	cookie = xcb_grab_pointer(xconn.conn, false, xconn.screen->root, MOUSE_MASK,
+				  XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, XCB_WINDOW_NONE,
+				  XCB_CURSOR_NONE, XCB_TIME_CURRENT_TIME);
+	reply = xcb_grab_pointer_reply(xconn.conn, cookie, &err);
+	if (err) error("couldn't grab pointer", xconn);
 	if (reply->status != XCB_GRAB_STATUS_SUCCESS)
-		return -1;
+		return false;
 
-	return 0;
+	return true;
 }
 
+void restore_cursor(x_connection_t xconn)
+{
+	xcb_font_t           font;
+	xcb_cursor_t         cursor;
+	xcb_gcontext_t       gc;
+	xcb_void_cookie_t    cookie;
+	xcb_generic_error_t *err;
+	uint32_t             vmask;
+	uint32_t             vlist[3];
 
+	font = xcb_generate_id(xconn.conn);
+	cookie = xcb_open_font_checked(xconn.conn, font, strlen("cursor"), "cursor");
+	err = xcb_request_check(xconn.conn, cookie);
+	if (err) error("couldn't load cursor font", xconn);
 
+	cursor = xcb_generate_id(xconn.conn);
+	cookie = xcb_create_glyph_cursor_checked(xconn.conn, cursor, font, font,
+						 CURSOR_CHAR, CURSOR_CHAR + 1, 0, 0, 0,
+						 UINT16_MAX, UINT16_MAX, UINT16_MAX);
+	err = xcb_request_check(xconn.conn, cookie);
+	if (err) error("couldn't create glyph cursor", xconn);
 
+	vmask = XCB_GC_FOREGROUND | XCB_GC_BACKGROUND | XCB_GC_FONT;
+	vlist[0] = xconn.screen->black_pixel;
+	vlist[1] = xconn.screen->white_pixel;
+	vlist[2] = font;
 
+	gc = xcb_generate_id(xconn.conn);
+	cookie = xcb_create_gc_checked(xconn.conn, gc, xconn.screen->root, vmask, vlist);
+	err = xcb_request_check(xconn.conn, cookie);
+	if (err) error("couldn't create graphics context", xconn);
+
+	cookie = xcb_change_window_attributes_checked(xconn.conn, xconn.screen->root,
+						      XCB_CW_CURSOR, &cursor);
+	err = xcb_request_check(xconn.conn, cookie);
+	if (err) error("xcb_change_window_attributes_checked()", xconn);
+}
 
 int main(int argc, char *argv[])
 {
-	xcb_connection_t     *conn;
-	xcb_screen_t         *screen;
-	int                   screen_num;
+	x_connection_t xconn;
 
+	/*
 
 	xcb_font_t            font;
 	xcb_cursor_t          cursor;
@@ -90,17 +136,18 @@ int main(int argc, char *argv[])
 	int                   cursor_glyph = 68;
 	uint32_t              vmask;
 	uint32_t              vlist[3];
+	*/
 
-	conn = connect(&screen_num);
-	screen = get_screen(conn, screen_num);
+	xconn = x_connect();
+	//grab_pointer(xconn);
+	restore_cursor(xconn);
+	x_disconnect(xconn);
 
-
-
+	return EXIT_SUCCESS;
 
 
 	/*
-	 * Hide the mouse
-	 */
+
 	pixmap = xcb_generate_id(conn);
 	cookie = xcb_create_pixmap_checked(conn, 1, pixmap, screen->root, 1, 1);
 	err = xcb_request_check(conn, cookie);
@@ -131,38 +178,10 @@ int main(int argc, char *argv[])
 
 	sleep(5);
 
-	/*
-	 * Show the mouse
-	 */
-	font = xcb_generate_id(conn);
-	cookie = xcb_open_font_checked(conn, font, strlen("cursor"), "cursor");
-	err = xcb_request_check(conn, cookie);
-	if (err) error("xcb_open_font_checked()", conn);
-
-	cursor = xcb_generate_id(conn);
-	cookie = xcb_create_glyph_cursor_checked(conn, cursor, font, font, cursor_glyph, cursor_glyph + 1,
-						 0, 0, 0, UINT16_MAX, UINT16_MAX, UINT16_MAX);
-	err = xcb_request_check(conn, cookie);
-	if (err) error("xcb_create_glyph_cursor_checked()", conn);
-
-	vmask = XCB_GC_FOREGROUND | XCB_GC_BACKGROUND | XCB_GC_FONT;
-	vlist[0] = screen->black_pixel;
-	vlist[1] = screen->white_pixel;
-	vlist[2] = font;
-
-	gc = xcb_generate_id(conn);
-	cookie = xcb_create_gc_checked(conn, gc, screen->root, vmask, vlist);
-	err = xcb_request_check(conn, cookie);
-	if (err) error("xcb_create_gc_checked()", conn);
-
-	vmask = XCB_CW_CURSOR;
-	cookie = xcb_change_window_attributes_checked(conn, screen->root, vmask, &cursor);
-	err = xcb_request_check(conn, cookie);
-	if (err) error("xcb_change_window_attributes_checked()", conn);
+	restore_cursor(conn);
 
 	xcb_disconnect(conn);
 
 	printf("Connects terminated\n");
-
-	return EXIT_SUCCESS;
+	*/
 }
